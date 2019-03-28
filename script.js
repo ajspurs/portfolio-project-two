a1161f4cfeed40a83c70fd8cc982539376bd58f3
Alex <spursalexj@hotmail.com>
2019-03-28 19:10:50 +0000

    gallery-features added

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block"; 
 }
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlidess");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1} 
+  x[slideIndex-1].style.display = "block"; 
+  setTimeout(carousel, 4000); // Change image every 2 seconds
+}
